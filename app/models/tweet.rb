d0ba10412ab6e52de2a60229d3d7f46e60a60766
Laura Purcell-Gates <laurapg1214@users.noreply.github.com>
2024-12-16 15:29:07 +0000

    image configs setup; all tests passing

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,9 @@
 class Tweet < ApplicationRecord
+  # associations
   belongs_to :user
+  has_one_attached :image
 
+  # validations
   validates :user, presence: true
   validates :message, presence: true, length: { maximum: 140 }
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Using the local settings for Active Storage
+  config.active_storage.service = :local
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
